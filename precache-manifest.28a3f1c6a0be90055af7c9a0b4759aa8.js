cd67e2cb22840096e983fd2cbad0c810ac41c018
swarnanagidi30 <swarnanagidi30@users.noreply.github.com>
2020-07-12 14:32:05 +0000

    deploy: fc4d2f9130f2d91c678156fcb9bd4dff5ab9e93c

---
+++ precache-manifest.28a3f1c6a0be90055af7c9a0b4759aa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3e51b800c56c47d3532cb85f7632b6c",
+    "url": "/sync-clock/index.html"
+  },
+  {
+    "revision": "bac93132984672100bac",
+    "url": "/sync-clock/static/css/main.23bebe20.chunk.css"
+  },
+  {
+    "revision": "38eef7370209d8479c1c",
+    "url": "/sync-clock/static/js/2.ae2bf730.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sync-clock/static/js/2.ae2bf730.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bac93132984672100bac",
+    "url": "/sync-clock/static/js/main.6565c729.chunk.js"
+  },
+  {
+    "revision": "8716354fa5cad5fe7314",
+    "url": "/sync-clock/static/js/runtime-main.294fc705.js"
+  }
+]);
